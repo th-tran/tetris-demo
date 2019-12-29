9271ae1c1f882d73e11f6c7dd2de0f7e2b1ce78f
Thomas Tran <tran.th.thomas@gmail.com>
2019-12-29 18:06:33 -0500

    Add T tetromino and basic block movement

---
+++ Tetris/Assets/TetrisBlock.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TetrisBlock : MonoBehaviour
+{
+    float previousTime;
+    float fallTime = 0.8f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            transform.position += new Vector3(-1, 0, 0);
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            transform.position += new Vector3(1, 0, 0);
+        }
+
+        if (Time.time - previousTime > fallTime)
+        {
+            transform.position += new Vector3(0, -1, 0);
+            previousTime = Time.time;
+        }
+    }
+}

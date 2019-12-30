0c37ca9f2f897b71d644d15970356b7603e8520b
Thomas Tran <tran.th.thomas@gmail.com>
2019-12-29 22:43:10 -0500

    Add properties to TetrisBlock to keep track of current state and status

--- Tetris/Assets/TetrisBlock.cs
+++ Tetris/Assets/TetrisBlock.cs
@@ -7,11 +7,23 @@ public class TetrisBlock : MonoBehaviour
     float previousTime;
     float fallTime = 0.8f;
     public Vector3 rotationPoint;
+    public enum State {
+        SPAWN,
+        RIGHT,
+        TWO,
+        LEFT
+    };
+    public State currentState;
+    public bool isSpecialBlock = false;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        currentState = State.SPAWN;
+        if (rotationPoint != Vector3.zero)
+        {
+            isSpecialBlock = true;
+        }
     }
 
     // Update is called once per frame
@@ -35,11 +47,7 @@ public class TetrisBlock : MonoBehaviour
         }
         else if (Input.GetKeyDown(KeyCode.UpArrow))
         {
-            transform.RotateAround(transform.TransformPoint(rotationPoint), new Vector3(0, 0, 1), -90);
-            if (!GameManager.Instance.ValidMove(transform))
-            {
-                GameManager.Instance.WallKick(transform);
-            }
+            AttemptRotation();
         }
 
         // Fall in increments of fallTime
@@ -55,6 +63,33 @@ public class TetrisBlock : MonoBehaviour
                 TetrominoSpawner.Instance.NewTetromino();
             }
             previousTime = Time.time;
+        }
+    }
+
+    void AttemptRotation()
+    {
+        transform.RotateAround(transform.TransformPoint(rotationPoint), new Vector3(0, 0, 1), -90);
+        if (!GameManager.Instance.ValidMove(transform))
+        {
+            GameManager.Instance.WallKick(transform);
+        }
+        // Update current block state
+        // TODO: Implement and account for left rotation
+        switch (currentState) {
+            case State.SPAWN:
+                currentState = State.RIGHT;
+                break;
+            case State.RIGHT:
+                currentState = State.TWO;
+                break;
+            case State.TWO:
+                currentState = State.LEFT;
+                break;
+            case State.LEFT:
+                currentState = State.SPAWN;
+                break;
+            default:
+                break;
         }
     }
 }

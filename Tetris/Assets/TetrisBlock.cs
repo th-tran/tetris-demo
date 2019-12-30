503feb5301f7516f5c9f618b8c6832ebeeb2607f
Thomas Tran <tran.th.thomas@gmail.com>
2019-12-29 19:20:14 -0500

    Add logic to keep blocks in bounds

--- Tetris/Assets/TetrisBlock.cs
+++ Tetris/Assets/TetrisBlock.cs
@@ -6,6 +6,9 @@ public class TetrisBlock : MonoBehaviour
 {
     float previousTime;
     float fallTime = 0.8f;
+    // Game boundaries
+    public static int height = 20;
+    public static int width = 10;
 
     // Start is called before the first frame update
     void Start()
@@ -19,16 +22,46 @@ public class TetrisBlock : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
             transform.position += new Vector3(-1, 0, 0);
+            if (!ValidMove())
+            {
+                transform.position -= new Vector3(-1, 0, 0);
+            }
         }
         else if (Input.GetKeyDown(KeyCode.RightArrow))
         {
             transform.position += new Vector3(1, 0, 0);
+            if (!ValidMove())
+            {
+                transform.position -= new Vector3(1, 0, 0);
+            }
         }
 
+        // Fall in increments of fallTime
         if (Time.time - previousTime > (Input.GetKey(KeyCode.DownArrow) ? fallTime / 10 : fallTime))
         {
             transform.position += new Vector3(0, -1, 0);
+            if (!ValidMove())
+            {
+                transform.position -= new Vector3(0, -1, 0);
+            }
             previousTime = Time.time;
         }
+    }
+
+    bool ValidMove()
+    {
+        // Iterate over each child and check if it is out of bounds
+        foreach (Transform child in transform)
+        {
+            int roundedX = Mathf.RoundToInt(child.transform.position.x);
+            int roundedY = Mathf.RoundToInt(child.transform.position.y);
+
+            if (roundedX < 0 || roundedX >= width || roundedY < 0 || roundedY >= height)
+            {
+                return false;
+            }
+        }
+
+        return true;
     }
 }

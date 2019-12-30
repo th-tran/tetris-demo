27a1ae23efe0344a044b5ca3aa0c7ef6c57fc93e
Thomas Tran <tran.th.thomas@gmail.com>
2019-12-30 00:50:07 -0500

    Update tetrominos, add sound manager

---
+++ Tetris/Assets/SoundManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    static SoundManager _instance;
+    public static SoundManager Instance { get { return _instance; } }
+    public AudioSource MusicSource;
+    public AudioClip bgMusic;
+
+    void Awake()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            _instance = this;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MusicSource.clip = bgMusic;
+        MusicSource.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}

9804b86c586ee58ccebdd7daab59850271ce383a
Thomas Tran <tran.th.thomas@gmail.com>
2019-12-29 21:26:52 -0500

    Add basic line clear logic

--- Tetris/Assets/TetrisBlock.cs
+++ Tetris/Assets/TetrisBlock.cs
@@ -54,6 +54,7 @@ public class TetrisBlock : MonoBehaviour
             {
                 transform.position -= new Vector3(0, -1, 0);
                 AddToGrid();
+                CheckForLines();
                 this.enabled = false;
                 TetrominoSpawner.Instance.NewTetromino();
             }
@@ -69,6 +70,59 @@ public class TetrisBlock : MonoBehaviour
             int roundedY = Mathf.RoundToInt(child.transform.position.y);
 
             grid[roundedX, roundedY] = child;
+        }
+    }
+
+    void CheckForLines()
+    {
+        for (int y = height-1; y >= 0; y--)
+        {
+            if (HasLine(y))
+            {
+                DeleteLine(y);
+                MoveRowDown(y);
+            }
+        }
+    }
+
+    bool HasLine(int y)
+    {
+        // Check for a full line
+        for (int x = 0; x < width; x++)
+        {
+            if (grid[x, y] == null)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    void DeleteLine(int y)
+    {
+        for (int x = 0; x < width; x++)
+        {
+            Destroy(grid[x, y].gameObject);
+            grid[x, y] = null;
+        }
+    }
+
+    void MoveRowDown(int y)
+    {
+        for (int newY = y; newY < height; newY++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                if (grid[x, newY] != null)
+                {
+                    // Move block down
+                    grid[x, newY-1] = grid[x, newY];
+                    grid[x, newY] = null;
+                    // Move grid down relatively
+                    grid[x, newY-1].transform.position -= new Vector3(0, 1, 0);
+                }
+            }
         }
     }
 
